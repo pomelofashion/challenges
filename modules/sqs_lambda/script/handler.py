19ba3f930efbbe11edfacc6c6816a76a7a232357
glefevre <glefevre@agoda.com>
2022-04-28 22:41:02 +0700

    Update Lambda script to fail when message id is greater than 1000

--- modules/sqs_lambda/script/handler.py
+++ modules/sqs_lambda/script/handler.py
@@ -3,8 +3,14 @@ import json
 def lambda_handler(event, context):
 
     response = {}
-    print(event)
+    # print(event)
+    
+    msg = json.loads(event['Records'][0]['body'])
 
+    if msg['id'] > 1000:
+        raise Exception(f"Message id {msg['id']} is too high")
+
+    print(msg)
     # Response Status
     statusCode = 200
     response["statusCode"] = statusCode
